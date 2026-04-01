0418bcb8bce5fe7968af95809b6a74034b37fb80
Eric Bartusch <ericbartusch8@gmail.com>
2026-03-31 23:08:58 -0500

    preview

--- GradientLineCode/Config.cs
+++ GradientLineCode/Config.cs
@@ -1,13 +1,41 @@
 ﻿using BaseLib.Config;
+using BaseLib.Config.UI;
+using Godot;
 
 namespace GradientLine.GradientLineCode;
 
 [HoverTipsByDefault]
-public class Config: SimpleModConfig
+public partial class Config : SimpleModConfig
 {
     public static GradientUtil.GradientType GradientType { get; set; } = GradientUtil.GradientType.Rainbow;
     public static bool Animate { get; set; } = true;
     [SliderRange(30, 200, 10)]
     public static double AnimateSpeed { get; set; } = 120f;
 
+    
+    public override void SetupConfigUI(Control optionContainer)
+    {
+        GenerateOptionsForAllProperties(optionContainer);
+        AddGradientPreview(optionContainer);
+        AddRestoreDefaultsButton(optionContainer);
+        
+    }
+    
+    protected void AddGradientPreview(Control optionContainer)
+    {
+        GradientPreviewControl gradientPreview = new GradientPreviewControl();
+        gradientPreview.CustomMinimumSize = new Vector2(120, 16);
+        gradientPreview.SetGradient(GradientUtil.BuildGradient(0f));
+        
+        ConfigChanged += (sender, args) =>
+        {
+            gradientPreview.SetGradient(
+                GradientUtil.BuildGradient(0f)
+            );
+        };
+        
+        optionContainer.AddChild(CreateSectionHeader("Preview"));
+        optionContainer.AddChild(gradientPreview);
+
+    }
 }
---
+++ GradientLineCode/GradientPreviewControl.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace GradientLine.GradientLineCode;
+
+public partial class GradientPreviewControl : Control
+{
+    private Gradient Gradient;
+    private float Thickness = 12f;
+    private GradientTexture2D _cachedTexture;
+
+    public override void _Draw()
+    {
+        if (Gradient == null)
+            return;
+
+        DrawTextureRect(_cachedTexture, new Rect2(Vector2.Zero, Size), false);
+    }
+
+    public void SetGradient(Gradient g)
+    {
+        Gradient = g;
+
+        _cachedTexture?.Dispose();
+        _cachedTexture = new GradientTexture2D
+        {
+            Gradient = g,
+            Width = 256,
+            Height = (int)Thickness
+        };
+
+        QueueRedraw();
+    }
+}
